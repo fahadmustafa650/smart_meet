87dd0bac897214ff82a19e6612af475ac7861e6a
akniaazi <ammarzebkn@gmail.com>
2021-06-01 11:49:35 +0500

    Visitor verification screens accomplished/added

---
+++ lib/Visitor/Visitor Verification Steps/qr_code_step1.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:smart_meet/widgets/custom_stepper.dart';
+
+import 'temperature_detector_step2.dart';
+
+class ScanQRCodeStep1 extends StatelessWidget {
+  static final id = '/scan_qrcode_step1';
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        title: Text('QR Code Vertification Step'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              icon: Icon(Icons.forward, color: Colors.white),
+              onPressed: () {
+                Navigator.pushNamed(context, TemperatureDetectionStep2.id);
+              })
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: screenHeight * 0.1,
+          ),
+          CustomStepper(
+            stepNo: 1,
+          ),
+          SizedBox(
+            height: screenHeight * 0.1,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+            child: Text(
+              'Put QR Code infront of Thermal Camera',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue),
+            ),
+          ),
+          SizedBox(
+            height: screenHeight * 0.1,
+          ),
+          Image(
+            width: screenWidth * 0.8,
+            height: screenHeight * 0.4,
+            image: AssetImage('assets/images/QR_Code.png'),
+          )
+        ],
+      ),
+    );
+  }
+}
